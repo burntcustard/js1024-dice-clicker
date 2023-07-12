2077f20e00d4f7798bda110d53bc240f24aaf593
John Evans <john@wearelighthouse.com>
2023-07-12 12:27:08 +0100

    Minor size optimizations

--- src/main.js
+++ src/main.js
@@ -59,42 +59,48 @@ const refreshShop = () => {
   [...shopSectionInner.children].map((item) => item.disabled = score < item.cost);
 };
 
-const buyItem = (item) => {
-  score -= item.cost;
-  scoreSectionInner.innerHTML = `•${score}`;
-  item.cost = ~~(item.cost*1.2);
-  item.costElement.innerHTML = `•${item.cost}`;
-  refreshShop();
-};
+const addNewShopItem = (icon, cost, buyCallback) => {
+  const item = document.createElement('button');
 
-const initDiceType = (
-  cost,
-  separator,
-  callback,
-) => {
-  const buyNewDice = document.createElement('button');
-  buyNewDice.cost = cost;
-  buyNewDice.style.cssText = `
+  item.cost = cost;
+  item.style.cssText = `
     display: flex;
     font: 32px system-ui;
     padding: 2px 8px;
     align-items: end;
   `;
-
-  buyNewDice.viewElement = document.createElement('div');
-  buyNewDice.viewElement.innerHTML = separator ? `⚅${separator}⚅` : '⚅';
-  buyNewDice.costElement = document.createElement('div');
-  buyNewDice.costElement.style.cssText = `
+  item.viewElement = document.createElement('div');
+  item.viewElement.innerHTML = icon;
+  item.costElement = document.createElement('div');
+  item.costElement.style.cssText = `
     font: 16px monospace;
     margin-left: auto;
   `;
-  buyNewDice.costElement.innerHTML = `•${buyNewDice.cost}`;
-  buyNewDice.append(buyNewDice.viewElement, buyNewDice.costElement);
-  shopSectionInner.append(buyNewDice);
-  buyNewDice.new = () => {
+  item.costElement.innerHTML = `•${item.cost}`;
+  item.append(item.viewElement, item.costElement);
+
+  item.onclick = () => {
+    score -= item.cost;
+    scoreSectionInner.innerHTML = `•${score}`;
+    item.cost = ~~(item.cost*1.2);
+    item.costElement.innerHTML = `•${item.cost}`;
+    refreshShop();
+    buyCallback();
+  }
+
+  shopSectionInner.append(item);
+};
+
+const initDiceType = (
+  cost,
+  separator,
+  diceCallback,
+) => {
+  const addNewDice = () => {
     const newDice = document.createElement('button');
-    // position: sticky is used in place of relative but saves 2B
-    // separator?.length + 1 is 1 or 2 (false + 1 = 1)
+
+      // position: sticky is used in place of relative but saves 2B
+      // separator?.length + 1 is 1 or 2 (false + 1 = 1)
     newDice.style.cssText = `
       display: flex;
       align-items: center;
@@ -106,21 +112,23 @@ const initDiceType = (
     newDice.inner1 = document.createElement('div');
     newDice.inner1.innerHTML = '⚅';
     newDice.inner1.style.cssText = `
-      display: inline-block;
+      display: inline-grid;
       transition: .5s all;
     `;
     newDice.append(newDice.inner1);
+
     if (separator) {
       newDice.inner2 = document.createElement('div');
       newDice.inner2.innerHTML = separator;
       newDice.inner3 = document.createElement('div');
       newDice.inner3.innerHTML = '⚅';
       newDice.inner3.style.cssText = `
-        display: inline-block;
+        display: inline-grid;
         transition: .5s all;
       `;
       newDice.append(newDice.inner2, newDice.inner3);
     }
+
     newDice.overlay = document.createElement('div');
     // 'red' uses reused characters so is < '#000', and the actual color
     // doesn't matter because it's always an emoji with it's own colors
@@ -131,13 +139,16 @@ const initDiceType = (
       font: 20px monospace;
     `;
     newDice.append(newDice.overlay);
+
     newDice.onclick = () => {
       newDice.disabled = true;
+
       diceFaces.concat(diceFaces).forEach((_, i) => setTimeout(() => {
         newDice.result1 = ~~(Math.random() * 6); // It's 0-indexed
         newDice.inner1.innerHTML = diceFaces[newDice.result1];
         newDice.inner1.style.rotate = `${Math.random()}turn`;
       }, 300 * i));
+
       if (separator) {
         diceFaces.concat(diceFaces).forEach((_, i) => setTimeout(() => {
           newDice.result2 = ~~(Math.random() * 6); // It's 0-indexed
@@ -147,45 +158,29 @@ const initDiceType = (
       }
       setTimeout(() => {
         newDice.disabled = false;
-        score += separator ? callback(newDice.result1 + 1, newDice.result2 + 1) : newDice.result1 + 1;
+        score += separator ? diceCallback(newDice.result1 + 1, newDice.result2 + 1) : newDice.result1 + 1;
         scoreSectionInner.innerHTML = `•${score}`;
         newDice.overlay.innerHTML = '';
         refreshShop();
       }, 4000);
     }
     diceSectionInner.append(newDice);
-  }
-  buyNewDice.onclick = () => {
-    buyItem(buyNewDice);
-    buyNewDice.new();
   };
 
-  return buyNewDice.new;
-}
+  addNewShopItem(
+    separator ? `⚅${separator}⚅` : '⚅',
+    cost,
+    addNewDice
+  );
+
+  return addNewDice;
+};
 
 initDiceType(6)();
 
-
-const buyNewRat = document.createElement('button');
-buyNewRat.cost = 24;
-buyNewRat.style.cssText = `
-  display: flex;
-  font: 32px system-ui;
-  padding: 2px 8px;
-  align-items: end;
-`;
-buyNewRat.viewElement = document.createElement('div');
-buyNewRat.viewElement.innerHTML = '🐀';
-buyNewRat.costElement = document.createElement('div');
-buyNewRat.costElement.style.cssText = `
-  font: 16px monospace;
-  margin-left: auto;
-`;
-buyNewRat.costElement.innerHTML = `•${buyNewRat.cost}`;
-buyNewRat.append(buyNewRat.viewElement, buyNewRat.costElement);
-buyNewRat.onclick = () => {
-  buyItem(buyNewRat);
+addNewShopItem('🐀', 24, () => {
   const newRat = document.createElement('div');
+
   newRat.style.cssText = `
     display: inline-grid;
     font: 32px system-ui;
@@ -193,13 +188,17 @@ buyNewRat.onclick = () => {
   `;
   newRat.innerHTML = '🐀';
   critterSectionInner.append(newRat);
+
   const nudgeDice = () => {
     const activeDice = [...diceSectionInner.children].filter(d => !d.disabled);
+
     if (activeDice.length) {
       const autoRollDice = activeDice[~~(Math.random() * activeDice.length)];
+
       autoRollDice.overlay.innerHTML = '🐀';
       autoRollDice.click();
       newRat.style.opacity = .5;
+
       setTimeout(() => {
         newRat.style.opacity = '';
       }, 4000);
@@ -207,8 +206,7 @@ buyNewRat.onclick = () => {
     setTimeout(nudgeDice, 8000);
   };
   setTimeout(nudgeDice, 800);
-}
-shopSectionInner.append(buyNewRat);
+});
 
 initDiceType(60, '+', (num1, num2) => num1 + num2);
 
